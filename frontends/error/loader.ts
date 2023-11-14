2dbe8c31aa1d4d7ce1d67d0afd2d82b9a44378d5
Betarost <betarost@yandex.ru>
2023-11-15 00:11:05 +0300

    .

--- frontends/error/loader.ts
+++ frontends/error/loader.ts
@@ -1,11 +1,10 @@
-import { Static, front } from "cemjs-all"
-
+import { Static, front,Fn } from "cemjs-all"
 export const loader = function () {
     Static.tt1 = 1
     setTimeout(() => {
         Static.tt1 = 5
-        console.log('=a775e6=', front.init)
-        front.init()
+        
+        Fn.init()
     }, 1500);
     return
 }
--- frontends/home/listener.ts
+++ frontends/home/listener.ts
@@ -1,13 +1,15 @@
+import { Static,Ref  } from "cemjs-all"
+
 export const finish = function (e) {
-  this.Static.sections = document.querySelectorAll('.section')
-  this.Static.navLinks = document.querySelectorAll('.header_navList-item')
-  this.Static.menu = document.querySelector('.header_navList')
-  this.Static.advantages = document.querySelector('.advantages_section')
+  Static.sections = document.querySelectorAll('.section')
+  Static.navLinks = document.querySelectorAll('.header_navList-item')
+  Static.menu = document.querySelector('.header_navList')
+  Static.advantages = document.querySelector('.advantages_section')
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        this.Static.navLinks.forEach((link: any) => {
+        Static.navLinks.forEach((link: any) => {
           const linkHref = link.getAttribute('href').replace('#', "")
           if (linkHref == entry.target.id) {
             link.classList.add('header_navList-item__active')
@@ -16,21 +18,21 @@ export const finish = function (e) {
           }
 
           if (entry.target.id == 'experts') {
-            this.Ref.scrollTop.classList.add('scrollTop__show')
+            Ref.scrollTop.classList.add('scrollTop__show')
           }
-          if (this.Static.advantages == entry.target) {
-            this.Ref.scrollTop.classList.remove('scrollTop__show')
+          if (Static.advantages == entry.target) {
+            Ref.scrollTop.classList.remove('scrollTop__show')
           }
 
           if (entry.target.id == 'program') {
-            this.Ref.question.classList.add('question__show')
+            Ref.question.classList.add('question__show')
           }
         })
       }
     })
   })
 
-  this.Static.sections.forEach(section => {
+  Static.sections.forEach(section => {
     observer.observe(section)
   })
 }
